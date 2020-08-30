8db7b0c4308adbf560fdbc2ae065fdf8bfa69323
TCGeneric <qoo0302@naver.com>
2020-08-30 19:49:36 +0900

    Made remove, edit, complete button work

--- Journey/public/js/interaction.js
+++ Journey/public/js/interaction.js
@@ -11,19 +11,27 @@ function createControlbox(id) {
     box.setAttribute("owner", id)
     let edit = document.createElement("i")
     edit.className = "fas fa-edit"
-    edit.addEventListener("click", function() {showEditModal(id)})
+    edit.addEventListener("click", function() {
+        showEditModal(id)
+    })
     let del = document.createElement("i")
     del.className = "fas fa-trash-alt"
-    del.onclick = showDeletebox(id)
+    del.addEventListener("click", function() {
+        showDeleteModal(id)
+    })
     let done = document.createElement("i")
     done.className = "fas fa-check"
-    done.addEventListener("click", function() {completeTodo(id) })
+    done.addEventListener("click", function() {
+        completeTodo(id)
+    })
     box.appendChild(edit)
     box.appendChild(del)
     box.appendChild(done)
     return box
 }
 
+// See this article about RESTful API: https://www.restapitutorial.com/lessons/httpmethods.html
+// Using fetch() method will be
 function showEditModal(id) {
     let todo = document.getElementById(id)
     let modal = document.getElementById("edit-modal")
@@ -35,10 +43,21 @@ function showEditModal(id) {
     let inputs = modal.querySelectorAll("input")
     inputs[0].value = todo.querySelector(".title").firstChild.textContent
     inputs[1].value = todo.querySelector(".subtitle").firstChild.textContent
+    // TODO: Send POST request here
 }
 
-function showDeletebox(id) {
-
+function showDeleteModal(id) {
+    let modal = document.getElementById("remove-modal")
+    modal.style.display = "block"
+    let close = modal.querySelector(".cancel")
+    close.addEventListener("click", function() {
+        modal.style.display = "none"
+    })
+    let close_btn = modal.querySelector(".cancel-btn")
+    close_btn.addEventListener("click", function() {
+        modal.style.display = "none"
+    })
+    // TODO: Send DELETE request here
 }
 
 function completeTodo(id) {
